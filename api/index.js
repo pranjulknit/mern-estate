0406554087ec089c9ba56137eef0f3a1a696bb1e
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2024-04-12 17:18:13 +0530

    create-a -testroute

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err.message);
 });
+
+app.use("api/user",userRouter);
 
 
 
