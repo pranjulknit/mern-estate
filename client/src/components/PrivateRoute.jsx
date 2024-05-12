ad8fcb21d28352560d603bd4550fe2cfcd5dbab8
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2024-05-13 03:46:49 +0530

    complete-profile-page-ui

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom';
+
+export default function PrivateRoute() {
+
+    // return <div>PrivateRoute</div>;
+
+    const {currentUser} = useSelector((state) => state.user);
+
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in"/>;
+  
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 export default function Profile() {
+  const {currentUser} = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"src={currentUser.avatar} alt="Profile"/>
+        <input type="text" placeholder="username" id="username" className="broder p-3 rounded-lg"/>
+        <input type="email" placeholder="email" id="email" className="broder p-3 rounded-lg"/>
+        <input type="password" placeholder="passwprd" id="password" className="broder p-3 rounded-lg"/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 ">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <button className="text-red-800 cursor-pointer ">
+          Delete account
+        </button>
+        <button className="text-red-800 p-3 cursor-pointer ">
+          Sign out
+        </button>
+      </div>
+    </div>
   )
 }
