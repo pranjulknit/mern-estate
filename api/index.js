51dddd7fc57399649dcb918507785b62aa9d67b8
Pranjul Chaurasiya <69967499+pranjulknit@users.noreply.github.com>
2024-05-15 04:32:44 +0530

    created-listing-api-route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -29,8 +30,9 @@ app.listen(3000,()=>{
 //     res.send('Hello World');
 // })
 
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
 app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
 
 app.use((err, req, res, next) => {
     
